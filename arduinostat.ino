c697bfdc18f8f223106ad17017f8dbd68fec6f8f
Subrata Sarker <subrata_sarker@yahoo.com>
2020-08-31 22:10:59 +0900

    Update script for zerostat

--- arduinostat.ino
+++ arduinostat.ino
@@ -1,20 +1,20 @@
-// simplestat
-// Based on the paper "Microcontroller based potentiostat"
-// DOI: 10.1021/acs.jchemed.5b00961 (J. Chem. Educ. 2016, 93, 1320−1322)
-// 
+// Project Name: zerostat
+// Version: 1.0
+// Description: Arduino Uno based potentiostat 
+// Author: Subrata Sarker <subrata_sarker@yahoo.com>
+// Date: 2020.08.31
+// License: MIT
 
 // PWM and ADC pin on arduino uno
-int DAC_OUT_R = 9;    // PWM
+int DAC_OUT_R = 9;      // PWM
 int DAC_OUT_W = 10;     // PWM
 
 float ADC_IN_C = A0;    // ADC (Current)
 float ADC_IN_V = A1;    // ADC (Voltage)
 
 float potstep = 0.019608; // fixed due to the DAC resolution (1/0.019608 == 51)
-int srs[] = {10,25,50,100,200,300}; //multiple scan rates values (mV/s)
-int const count = 6;
 int const data_count = 10;
-long intervals[count];
+long interval;
 int cdata[data_count];
 float avg_data;
 
@@ -27,8 +27,9 @@ int inputs = 0;
 String inputstr;
 
 unsigned int halt;            // 0 for stop and 1 for run       
-unsigned int sr;              // 0, 1, 2, 3, 4, and 5 
+unsigned int sr;              // 5 to 300 mV       
 unsigned int mode;            // 0 for lsv and 1 for cv
+int ncycles;        // number of cycles from 0 to onward
 int pcom;            // should be smaller than end point (5, 250)
 int pstart;          // should be smaller than end point (0, 255)
 int pend;            // should be larger than start point (0, 255)
@@ -57,12 +58,6 @@ void setup() {
   
   // reset val
   val = 0;
-    
-  //  populate intervalos
-  for(int pos = 0; pos < count; pos++)
-  {
-    intervals[pos] = 1000000L /(srs[pos] * 51L);
-  }
 
   Serial.println("ready,,,,,,");
   delay(500);
@@ -79,48 +74,35 @@ void loop() {
   analogWrite(DAC_OUT_W, 127); // set potential
   
   inputs = get_inputs();
-  if (inputs > 0 && halt == 0 && pend > pstart) {
+  if (inputs > 0 && halt == 0) {
     inputs = 0;
-
+    interval = 1000000L /(sr * 51L);
     switch(mode) {
-      // if mode = 0 then forward scan
+      // if mode = 0 then LSV
       case 0:
-        forwardScan(sr);
-        broadcast(0, false);
-        break;
-
-      // if mode = 1 then reverse scan
-      case 1:
-        reverseScan(sr);
-        broadcast(0, false);
-        break;
-
-      // if mode = 2 then reverse scan follows forward scan (cycle)
-      case 2:
-        forwardScan(sr);
-        reverseScan(sr);
-        broadcast(0, false);
-        break;
-
-      // if mode = 3 then forward scan follows reverse scan (cycle)
-      case 3:
-        reverseScan(sr);
-        forwardScan(sr);
-        broadcast(0, false);
-        break;
-
-      // if mode = 4 or more then reverse scan follows forward scan ((mode -2)  no. of cycles)
-      default:
-        for (int i = 0; i < mode - 2; i++) {
-          forwardScan(sr);
-          reverseScan(sr);
-          broadcast(0, false);
+        if (pstart < pend) {
+          forwardScan(interval);
+        } else {
+          reverseScan(interval);
         }
+        broadcast(false);
+        break;
+
+      // if mode = 1 then CV
+      case 1:
+        for (int i = 0; i < ncycles; i++) {
+          if (pstart < pend) {
+            forwardScan(interval);
+            reverseScan(interval);
+          }
+        }
+        broadcast(false);
+        break;
     }
   }  
 }
 
-void forwardScan(unsigned int pos) {
+void reverseScan(unsigned int sr) {
   for(val = pstart; val <= pend; val++)
   {
     inputs = get_inputs();
@@ -131,7 +113,7 @@ void forwardScan(unsigned int pos) {
     }
     analogWrite(DAC_OUT_R,val); // set potential and
     analogWrite(DAC_OUT_W,pcom); // set potential and
-    delay(intervals[pos]); // wait
+    delay(interval); // wait
 
     // get data and average
     for (int i = 0; i < data_count && halt == 0; i++) {
@@ -141,12 +123,12 @@ void forwardScan(unsigned int pos) {
     avg_data = average(cdata, data_count);
     
     // send data to the serial    
-    broadcast(pos, true);
+    broadcast(true);
   }
 }
 
 
-void reverseScan(unsigned int pos) {
+void forwardScan(unsigned int sr) {
   for(val = pend; val >= pstart; val--)
   {
     inputs = get_inputs();
@@ -158,7 +140,7 @@ void reverseScan(unsigned int pos) {
     
     analogWrite(DAC_OUT_R,val);
     analogWrite(DAC_OUT_W,pcom); // set potential and
-    delay(intervals[pos]); // wait
+    delay(interval); // wait
 
     // get data and average
     for (int i = 0; i < data_count; i++) {
@@ -168,7 +150,7 @@ void reverseScan(unsigned int pos) {
     avg_data = average(cdata, data_count);
 
     // send data to the serial
-    broadcast(pos, true);
+    broadcast(true);
   }
 }
 
@@ -180,7 +162,7 @@ float average(int numbers[], int count) {
   return ((float)sum / count);
 }
 
-void broadcast(int pos, bool is_running) {
+void broadcast(bool is_running) {
   (is_running) ? Serial.print(1) : Serial.print(0);
   Serial.print(",");
   Serial.print(val);
@@ -189,9 +171,9 @@ void broadcast(int pos, bool is_running) {
   Serial.print(",");
   Serial.print(v);
   Serial.print(",");
-  Serial.print(srs[pos]);
+  Serial.print(sr);
   Serial.print(",");  
-  Serial.print(intervals[pos]);
+  Serial.print(interval);
   Serial.print(",");
   Serial.println();
 }
@@ -211,8 +193,8 @@ int get_inputs() {
       }
     }
     
-    // s => "sr,halt,mode,pstart,pend"
-    result = sscanf(inputstr.c_str(), "%u,%u,%u,%d,%d,%d\r", &sr, &halt ,&mode, &pcom, &pstart, &pend);
+    // s => "sr,halt,mode, ncycles, pcom, pstart,pend"
+    result = sscanf(inputstr.c_str(), "%u,%u,%u,%d,%d,%d,%d\r", &sr, &halt ,&mode, &ncycles, &pcom, &pstart, &pend);
   }
   
   return result;
